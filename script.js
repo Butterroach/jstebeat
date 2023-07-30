06b2090191d06bf8ba5f86f9b86484327932e35d
Butterroach <eforevernof@gmail.com>
2023-07-30 18:55:36 +0300

    rewrote the entirety of script.js
    now it works yipee
    btw still uses deprecated methods but eh, who cares

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
-let audioContext;
-let audioBuffer;
-let bytebeatNode;
-let isPlaying = false;
-
 function playBytebeat() {
-  // get the bytebeat code from the HTML text form
+  E = Math.E;
+  PI = Math.PI;
+  abs = Math.abs;
+  acos = Math.acos;
+  cbrt = Math.cbrt;
+  ceil = Math.ceil;
+  cos = Math.cos;
+  exp = Math.exp;
+  floor = Math.floor;
+  log = Math.log;
+  max = Math.max;
+  min = Math.min;
+  random = Math.random;
+  round = Math.round;
+  sin = Math.sin;
+  sqrt = Math.sqrt;
+  tan = Math.tan;
   const bytebeatCode = document.getElementById("bytebeat-code").value;
-
-  // create an audio context
-  audioContext = new AudioContext();
-
-  // get the sample rate from the HTML input
   const sampleRate = document.getElementById("sample-rate").value;
-  audioContext.sampleRate = sampleRate;
-
-  console.log("this shi playin"); // * debug, remove before production
-  console.log(sampleRate); // * debug, remove before production
-
-  // create a function from the bytebeat code and compile it for faster execution
-  const bytebeat = new Function("t", "return (" + bytebeatCode + ")").bind(
-    null
-  );
-
-  // create a script processor node to generate audio data
-  bytebeatNode = audioContext.createScriptProcessor(0, 1, 1);
-
-  bytebeatNode.onaudioprocess = function (event) {
-    const outputBuffer = event.outputBuffer;
-    const outputChannel = outputBuffer.getChannelData(0);
-    for (let i = 0; i < outputBuffer.length; i++) {
-      // calculate the time for each sample based on the current position in the buffer
-      const t = (i + audioContext.currentTime * sampleRate) / sampleRate;
-      // call the bytebeat code to generate the next sample value
-      const sampleValue = bytebeat(t);
-      // convert the sample value to an 8-bit unsigned integer
-      const byteValue = sampleValue & 255;
-      // normalize the sample value to the range -1 to 1 and write it to the output buffer
-      outputChannel[i] = byteValue / 128 - 1;
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const bufferSize = 2 ** 14;
+  const scriptNode = audioContext.createScriptProcessor(bufferSize, 0, 1);
+  let theReali = 0;
+  scriptNode.onaudioprocess = function (audioProcessingEvent) {
+    const outputBuffer = audioProcessingEvent.outputBuffer.getChannelData(0);
+    for (let i = 0; i < bufferSize; i++) {
+      theReali++;
+      t = (theReali / audioContext.sampleRate) * sampleRate;
+      const x = eval(bytebeatCode) & 255;
+      const sample = x / 128 - 1;
+      outputBuffer[i] = sample;
+      setTimeout(null, 1);
     }
   };
 
-  // connect the bytebeat node to the audio context's destination
-  bytebeatNode.connect(audioContext.destination);
-
-  // start playing the audio
-  isPlaying = true;
-  audioContext.resume();
+  scriptNode.connect(audioContext.destination);
 }
 
 function stopBytebeat() {
-  // disconnect the bytebeat node from the audio context's destination
-  bytebeatNode.disconnect();
-  isPlaying = false;
+  audioContext.suspend();
 }
