4c48942509ba70fa550c26c55b5cc49437502bb4
Butterroach <butterroach@proton.me>
2023-08-31 07:07:38 +0300

    add an option to reset the background color

--- script.js
+++ script.js
@@ -21,6 +21,13 @@ function updateBackground() {
   localStorage.setItem("backgroundColor", color);
 }
 
+function resetBackground() {
+  const color = "#1e1e1e";
+  document.getElementById("background-color").value = color;
+  document.body.style.backgroundColor = color;
+  localStorage.setItem("backgroundColor", color);
+}
+
 function base64ToBytes(base64) {
   const binString = atob(base64);
   return Uint8Array.from(binString, (m) => m.codePointAt(0));
